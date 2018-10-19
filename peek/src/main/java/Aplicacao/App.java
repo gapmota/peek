b5391fa8b2beb4bf147ab8b992a45e6406945ab8
Aluno bandtec <github@alunos.bandtec.com.br>
2018-10-19 18:45:20 -0300

    0.2 | oshi 3.9

--- peek/src/main/java/Aplicacao/App.java
+++ peek/src/main/java/Aplicacao/App.java
@@ -2,53 +2,26 @@ package Aplicacao;
 
 import java.io.Console;
 
+import oshi.SystemInfo;
+
 public class App {
 	public static void main(String[] args) {
 		
-		System.out.println(resumo());
-		
-	}
-	
-	
-	public static String resumo() {
-		String rs = "";
-		
 		Computador c = new Computador();
 		
-		rs = c.getProcessador().getNomeProcessador()+"\n";
-		rs += c.getProcessador().getResumoProcessador()+"\n";
-		rs += c.getProcessador().getPorcetagemDeUso()+"\n";
-		rs += c.getProcessador().getTemperatura()+"\n";
-		rs += c.getProcessador().getVelocidadeFans()+"\n";
-		rs += c.getProcessador().getVoltagem()+"\n";
-		rs += "\n-------------------------------------------\n";
-		rs += "\n-------------------------------------------\n";
-		rs += "\n-------------------------------------------\n";
-		rs += c.getHD().getArvore()+"\n";
-		rs += c.getHD().getNomeParticao(0, 1)+"\n";
-		rs += c.getHD().getEspacoDisponivel(0)+"\n";
-		rs += c.getHD().getEspacoTotal(0)+"\n";
-		rs += c.getHD().getEspacoTotalParticao(0, 0);
-		rs += c.getHD().getEspacoUsado(0);
-		rs += c.getHD().getLeitura(0);
-		rs += c.getHD().getQuantidadeDisco();
-		rs += "\n-------------------------------------------\n";
-		rs += "\n-------------------------------------------\n";
-		rs += "\n-------------------------------------------\n";
-		rs += c.getRam().getDisponivel()+"\n";
-		rs += c.getRam().getTotal()+"\n";
-		rs += c.getRam().getUsando()+"\n";
-		rs += "\n-------------------------------------------\n";
-		rs += "\n-------------------------------------------\n";
-		rs += "\n-------------------------------------------\n";
-		rs += c.getRede().getAdaptadorDeRede(1)+"\n";
-		rs += c.getRede().getIPv4(1)+"\n";
-		rs += c.getRede().getIPv6(1)+"\n";
-		rs += c.getRede().getMAC(1)+"\n";
-		rs += c.getRede().getTotalInterfaces()+"\n";
-		rs += c.getRede().getVelocidade(1);
+		String[] mac = c.getRede().getMacsPC();
 		
-		return rs;
+		for(int i = 0; i < mac.length; i++) {
+			System.out.println(mac[i]);
+		}
+		
+		System.out.println();
+		
+		System.out.println(c.getRede().getMAC());
+		System.out.println(c.getRede().getVelocidadeDownload());
+		System.out.println(c.getRede().getVelocidadeUpload());
+		
+		System.out.println(new SystemInfo().getHardware().getNetworkIFs()[0].getBytesRecv());
+		
 	}
-	
 }
--- peek/src/main/java/Aplicacao/Rede.java
+++ peek/src/main/java/Aplicacao/Rede.java
@@ -3,24 +3,67 @@ package Aplicacao;
 import java.net.SocketException;
 
 import oshi.SystemInfo;
+import oshi.util.FormatUtil;
 
 public class Rede {
-	private SystemInfo systemInfo = new SystemInfo();
+	private SystemInfo systemInfo = null;
 	
 	/**
 	 * @return Retorna a velocidade da rede em bits
 	 */
 	
 	public int getTotalInterfaces() {
+		systemInfo = new SystemInfo();
 		return systemInfo.getHardware().getNetworkIFs().length;
 	}
 	
-	public long getVelocidade(int index) {		
-		return systemInfo.getHardware().getNetworkIFs()[index].getSpeed();		
+	public String[] getMacsPC() {
+		systemInfo = new SystemInfo();
+		systemInfo.getHardware().getNetworkIFs()[0].getMacaddr();
+		
+		int index = systemInfo.getHardware().getNetworkIFs().length;
+		
+		String[] macs = new String[index];
+		
+		for(int i = 0; i < index; i++) {
+			macs[i] = systemInfo.getHardware().getNetworkIFs()[i].getMacaddr();
+		}
+		
+		return macs;
+	}
+	
+	public String getVelocidadeDownload() {
+		systemInfo = new SystemInfo();
+		
+		systemInfo.getHardware()
+			.getNetworkIFs()[getInterfaceEmUso()]
+					.updateNetworkStats();
+		
+		return FormatUtil.formatBytes(systemInfo.getHardware()
+				.getNetworkIFs()[getInterfaceEmUso()]
+						.getBytesSent());
+	}
+	
+	public String getVelocidadeUpload() {
+		systemInfo = new SystemInfo();
+		
+		systemInfo.getHardware()
+		.getNetworkIFs()[getInterfaceEmUso()]
+				.updateNetworkStats();
+		
+		return FormatUtil.formatBytes(systemInfo.getHardware()
+				.getNetworkIFs()[getInterfaceEmUso()]
+						.getBytesRecv());
+	}
+	
+	public long getVelocidadeInterface() {	
+		systemInfo = new SystemInfo();
+		return systemInfo.getHardware().getNetworkIFs()[getInterfaceEmUso()].getSpeed();		
 	}
 
-	public String getIPv4(int index) {
-		String[] redes = systemInfo.getHardware().getNetworkIFs()[index].getIPv4addr();
+	public String getIPv4() {
+		systemInfo = new SystemInfo();
+		String[] redes = systemInfo.getHardware().getNetworkIFs()[getInterfaceEmUso()].getIPv4addr();
 		String infos = "";
 		for(int i = 0; i < redes.length; i++) {
 			infos += redes[i];
@@ -28,8 +71,9 @@ public class Rede {
 		return infos;
 	}
 	
-	public String getIPv6(int index) {
-		String[] redes = systemInfo.getHardware().getNetworkIFs()[index].getIPv6addr();
+	public String getIPv6() {
+		systemInfo = new SystemInfo();
+		String[] redes = systemInfo.getHardware().getNetworkIFs()[getInterfaceEmUso()].getIPv6addr();
 		String infos = "";
 		for(int i = 0; i < redes.length; i++) {
 			infos += redes[i];
@@ -37,13 +81,26 @@ public class Rede {
 		return infos;
 	}
 	
-	public String getMAC(int index) {		
-		return systemInfo.getHardware().getNetworkIFs()[index].getMacaddr();		
+	public String getMAC() {		
+		systemInfo = new SystemInfo();
+		return systemInfo.getHardware().getNetworkIFs()[getInterfaceEmUso()].getMacaddr();		
 	}
 
-	public String getAdaptadorDeRede(int index) {
-		return systemInfo.getHardware().getNetworkIFs()[index].getDisplayName();
+	public String getAdaptadorDeRede() {
+		systemInfo = new SystemInfo();
+		return systemInfo.getHardware().getNetworkIFs()[getInterfaceEmUso()].getDisplayName();
 	}
 	
+	
+	private int getInterfaceEmUso() {
+		systemInfo = new SystemInfo();
+		
+		for(int i = 0; i < systemInfo.getHardware().getNetworkIFs().length; i++) {
+			if(!systemInfo.getHardware().getNetworkIFs()[i].getDisplayName().trim().equals(""))
+				return i;
+		}
+		
+		return -1;
+	}
 	
 }
