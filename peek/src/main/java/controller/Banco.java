becadee792abc589edc51722d9751d22bd43f766
mateuscovos <30701583+mateuscovos@users.noreply.github.com>
2018-10-26 19:34:35 -0300

    Update Banco.java

--- peek/src/main/java/controller/Banco.java
+++ peek/src/main/java/controller/Banco.java
@@ -6,9 +6,7 @@ import java.sql.SQLException;
 
 public class Banco {
 
-    private static String PATH_SQL = "jdbc:sqlserver://mateuzserver.database.windows.net:1433;database=MEU;"
-            + "user=mateuz@mateuzserver;password=Banco2k18;encrypt=true;trustServerCertificate=false;"
-            + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+    private static String PATH_SQL = "";
 
     public Connection getInstance() {
         try {
