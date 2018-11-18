f882521cd309bda5f7df65db1c1d32519bd2c1f6
mateuscovos <30701583+mateuscovos@users.noreply.github.com>
2018-11-18 00:52:49 -0200

    api retornando json

--- peekapi/peekapi/Dao/Banco.cs
+++ peekapi/peekapi/Dao/Banco.cs
@@ -11,7 +11,7 @@ namespace peekapi.Dao
     public class Banco
     {
        // private static readonly string stringCnx = ConfigurationManager.ConnectionStrings["conexao"].ConnectionString;
-        public string StringDeConexao = "Server = tcp:mateuzserver.database.windows.net,1433; Initial Catalog = MEU; Persist Security Info = False; User ID = mateuz; Password = Banco2k18; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+        public string StringDeConexao = "Server = tcp:x.database.windows.net,1433; Initial Catalog = x; Persist Security Info = False; User ID = x; Password = x; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
         public SqlConnection PegarConexao()
         {
             using (SqlConnection cnx = new SqlConnection(StringDeConexao))
