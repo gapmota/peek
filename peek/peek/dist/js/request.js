aaf942715de07e0124c9ac71f32b37e5b93b0890
Aluno Bandtec <github@alunos.bandtec.com.br>
2018-11-23 19:22:08 -0200

    2.0.1.5 | js request quase terminados

--- peek/peek/dist/js/request.js
+++ peek/peek/dist/js/request.js
@@ -3,8 +3,8 @@ let idLab;
 
 const api_url = "http://visionpeekapi.azurewebsites.net/peek/";
 
-function setIdUsuario(id){
-    if(idUsuarioLogado == undefined){
+function setIdUsuario(id) {
+    if (idUsuarioLogado == undefined) {
         idUsuarioLogado = id;
     }
 }
@@ -15,41 +15,41 @@ function setIdLab(id) {
     }
 }
 
-function pegarConsumoDownloadUploadLaboratorios(download,upload,idUsuario){    
+function pegarConsumoDownloadUploadLaboratorios(download, upload, idUsuario) {
     $.ajax({
         type: 'GET',
         dataType: 'json',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        url: api_url+"Rede/?idUsuario="+idUsuario,
+        url: api_url + "Rede/?idUsuario=" + idUsuario,
         data: '',
         success: function (response) {
             download.text(response.Download);
-            upload.text(response.Upload);        
+            upload.text(response.Upload);
         },
         error: function () {
-            
+
         }
     });
 
 }
 
-function pegarQuantidadeDeLaboratorios(laboratorio, idUsuario){
+function pegarQuantidadeDeLaboratorios(laboratorio, idUsuario) {
     $.ajax({
         type: 'GET',
         dataType: 'json',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        url: api_url +"Laboratorio/?oque=quantidade&idUsuario="+idUsuario,
+        url: api_url + "Laboratorio/?idUsuario=" + idUsuario,
         data: '',
         success: function (response) {
-            console.log(response);  
-            laboratorio.text(response);    
+            console.log(response);
+            laboratorio.text(response);
         },
         error: function () {
-            
+
         }
     });
 }
@@ -61,10 +61,10 @@ function pegarQuantidadeDePC(idUsuario) {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        url: api_url +"Computador?idUsuario=" + idUsuario,
+        url: api_url + "Computador?idUsuario=" + idUsuario,
         data: '',
         success: function (response) {
-            console.log(response);            
+            console.log(response);
         },
         error: function () {
 
@@ -79,7 +79,7 @@ function pegarProcessosQueMaisConsumemHardware() {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        url: api_url + "Processo?idUsuario=" + idUsuario +"&oque=labUsando",
+        url: api_url + "Processo?idUsuario=" + idUsuario + "&oque=labUsando",
         data: '',
         success: function (response) {
             console.log(response);
@@ -108,19 +108,150 @@ function pegarProcessosQueMaisConsumemInternet() {
     });
 }
 
+function pegarTodosLaboratorioDeUmUsuario(idUsuario) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: api_url + "Rede?idUsuario=" + idUsuario + "&oque=labs",
+        data: '',
+        success: function (response) {
+            console.log(response);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function pegarTodosLabs(idUsuario) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: api_url + "Laboratorio/?oque=labs&idUsuario=" + idUsuario,
+        data: '',
+        success: function (response) {
+            console.log(response);
+            laboratorio.text(response);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function pegarInformacoesPC(idComputador) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: api_url + "Computador/?contexto=individual&id=" + idComputador,
+        data: '',
+        success: function (response) {
+            console.log(response);
+            laboratorio.text(response);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function pegarInformacoesCompletasPC(idComputador) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: api_url + "InfoComputador/?idComputador=" + idComputador,
+        data: '',
+        success: function (response) {
+            console.log(response);
+            laboratorio.text(response);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function pegarInformacoesPcPorLab(idLab) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: api_url + "Computador/?contexto=laboratario&id=" + idLab,
+        data: '',
+        success: function (response) {
+            console.log(response);
+            laboratorio.text(response);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function pegarMediaPorcetagemUsoComputador(idUsuario) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: api_url + "Processador/?idUsuario=" + idUsuario,
+        data: '',
+        success: function (response) {
+            console.log(response);
+            laboratorio.text(response);
+        },
+        error: function () {
+
+        }
+    });
+}
 
 
-window.onload = function(){//ao carregar a pagina
+function pegarLaboratoriosQueMaisConsomem(idUsuario) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: api_url + "Rede/?idUsuario=" + idUsuario + "&oque=consumoPorLab",
+        data: '',
+        success: function (response) {
+            console.log(response);
+            laboratorio.text(response);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+window.onload = function () {//ao carregar a pagina
     setIdUsuario(4);
-    pegarConsumoDownloadUploadLaboratorios($("#txtDownload"),$("#txtUpload"),idUsuarioLogado);
-    pegarQuantidadeDeLaboratorios($("#txtQuantidadeLaboratorio"),idUsuarioLogado);
+    pegarConsumoDownloadUploadLaboratorios($("#txtDownload"), $("#txtUpload"), idUsuarioLogado);
+    pegarQuantidadeDeLaboratorios($("#txtQuantidadeLaboratorio"), idUsuarioLogado);
 };
 
-setInterval(function (){//um minuto
-    pegarConsumoDownloadUploadLaboratorios($("#txtDownload"),$("#txtUpload"),idUsuarioLogado);
-    
-},10000);
+setInterval(function () {//um minuto
+    pegarConsumoDownloadUploadLaboratorios($("#txtDownload"), $("#txtUpload"), idUsuarioLogado);
 
-setInterval(function (){//dez minutos
-    pegarQuantidadeDeLaboratorios($("#txtQuantidadeLaboratorio"),idUsuarioLogado);
-},100000);
+}, 10000);
+
+setInterval(function () {//dez minutos
+    pegarQuantidadeDeLaboratorios($("#txtQuantidadeLaboratorio"), idUsuarioLogado);
+}, 100000);
